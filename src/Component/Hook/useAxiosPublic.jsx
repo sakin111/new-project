0adf62168c50984cfbc4cf67fd58edc303ce306b
sakin111 <maliksakin53@gmail.com>
2024-09-02 12:06:12 +0600

    added a link

--- src/Component/Hook/useAxiosPublic.jsx
+++ src/Component/Hook/useAxiosPublic.jsx
@@ -1,39 +1,14 @@
 import axios from "axios";
-import { useMemo } from "react";
 
-const createAxiosInstance = () => {
-  const axiosInstance = axios.create();
 
-  axiosInstance.interceptors.response.use(
-    (response) => {
-      // If the response is successful, return it
-      return response;
-    },
-    async (error) => {
-      // If the request fails and it's a network error, switch to production URL
-      if (error.code === "ERR_NETWORK") {
-        axiosInstance.defaults.baseURL = "https://new-project-server-hvl1awoyf-maliksakin53gmailcoms-projects.vercel.app/";
-        try {
-          // Retry the request with the production URL
-          return await axiosInstance.request(error.config);
-        } catch (retryError) {
-          return Promise.reject(retryError);
-        }
-      }
-      return Promise.reject(error);
-    }
-  );
+const axiosPublic = axios.create({
+    // baseURL:'http://localhost:5000'
+    baseURL:'https://new-project-server-hvl1awoyf-maliksakin53gmailcoms-projects.vercel.app/'
+})
 
-  // Initially, try to use localhost
-  axiosInstance.defaults.baseURL = "http://localhost:5000";
-
-  return axiosInstance;
-};
 
 const useAxiosPublic = () => {
-  const axiosInstance = useMemo(() => createAxiosInstance(), []);
-  return axiosInstance;
+    return axiosPublic;
 };
 
 export default useAxiosPublic;
-
