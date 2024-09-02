cca40849ff07b51814bfbaeb1276f9d8f2e98d12
sakin111 <maliksakin53@gmail.com>
2024-09-02 11:29:44 +0600

    added a production link

--- src/Component/Hook/useAxiosPublic.jsx
+++ src/Component/Hook/useAxiosPublic.jsx
@@ -1,13 +1,26 @@
 import axios from "axios";
 
+const checkLocalhost = async () => {
+  try {
+    // Try to ping the localhost server
+    await axios.get("http://localhost:5000");
+    return "http://localhost:5000";
+  } catch (error) {
+    // If the localhost server is not reachable, fall back to the production URL
+    return "https://new-project-server-l5jy9b984-maliksakin53gmailcoms-projects.vercel.app/";
+  }
+};
 
-const axiosPublic = axios.create({
-    baseURL:'http://localhost:5000'
-})
+const createAxiosInstance = async () => {
+  const baseURL = await checkLocalhost();
+  return axios.create({
+    baseURL: baseURL,
+  });
+};
 
-
-const useAxiosPublic = () => {
-    return axiosPublic;
+const useAxiosPublic = async () => {
+  const axiosInstance = await createAxiosInstance();
+  return axiosInstance;
 };
 
 export default useAxiosPublic;
