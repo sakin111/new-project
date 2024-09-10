7665944843ad460174c8363d9f9d4174abf46d96
sakin111 <maliksakin53@gmail.com>
2024-09-10 11:38:37 +0600

    git commited

--- src/Component/Hook/useAxiosPublic.jsx
+++ src/Component/Hook/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     // baseURL:'http://localhost:5000'
-    baseURL:'https://new-project-server-hvl1awoyf-maliksakin53gmailcoms-projects.vercel.app/'
+    baseURL:'https://new-project-server-maliksakin53gmailcoms-projects.vercel.app/'
  
 });
 
--- src/Component/Hook/useAxiosSecure.jsx
+++ src/Component/Hook/useAxiosSecure.jsx
@@ -14,6 +14,7 @@ const useAxiosSecure = () => {
 
 axiosSecure.interceptors.request.use((config) => {
     const token = localStorage.getItem('access-token');
+    console.log('Token:', token); 
     if (token) {
         config.headers.authorization = `Bearer ${token}`;
     }
